baea14d783d9273b4d4da0228f5d87a5e3236de1
Disservin <disservin.social@gmail.com>
2025-04-13 13:23:36 +0200

    Update min GCC to 9.3 and Clang to 10.0

--- worker/worker.py
+++ worker/worker.py
@@ -63,10 +63,10 @@ from updater import update
 LOCK_FILE = Path(__file__).resolve().parent / "fishtest_worker.lock"
 
 # Minimum requirement of compiler version for Stockfish.
-MIN_GCC_MAJOR = 7
+MIN_GCC_MAJOR = 9
 MIN_GCC_MINOR = 3
 
-MIN_CLANG_MAJOR = 8
+MIN_CLANG_MAJOR = 10
 MIN_CLANG_MINOR = 0
 
 FASTCHESS_SHA = "f8e58066992e5e130f07fb3ba49b89b603e32f21"
