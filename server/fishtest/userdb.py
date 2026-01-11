d9bda5fae2794ea8d67be8e18f353749ca3d2909
Disservin <disservin.social@gmail.com>
2026-01-11 16:18:38 +0100

    fix import

--- server/fishtest/userdb.py
+++ server/fishtest/userdb.py
@@ -2,7 +2,8 @@ import secrets
 import sys
 from datetime import UTC, datetime
 
-from fishtest.lru_cache import lru_cache, user_schema
+from fishtest.lru_cache import lru_cache
+from fishtest.schemas import user_schema
 from pymongo import ASCENDING
 from vtjson import ValidationError, validate
 
